a7b9eeb04c894434df28e50c1717df956cf6f9b1
Wekend <wekendmc@gmail.com>
2025-11-23 15:39:53 -0800

    silly mistakes

--- src/main/kotlin/llc/redstone/systemapi/SystemAPI.kt
+++ src/main/kotlin/llc/redstone/systemapi/SystemAPI.kt
@@ -8,14 +8,14 @@ import org.slf4j.LoggerFactory
 
 object SystemAPI : ClientModInitializer {
     val MOD_ID = "systemapi"
-    val LOGGER: Logger = LoggerFactory.getLogger("Housing Toolbox")
+    val LOGGER: Logger = LoggerFactory.getLogger("SystemAPI")
     val VERSION = /*$ mod_version*/ "0.2.3";
     val MINECRAFT = /*$ minecraft*/ "1.21.9";
     val MC: MinecraftClient
         get() = MinecraftClient.getInstance()
 
     override fun onInitializeClient() {
-        LOGGER.info("Loaded Housing Toolbox v$VERSION for Minecraft $MINECRAFT.")
+        LOGGER.info("Loaded v$VERSION for Minecraft $MINECRAFT.")
 
         mcCoroutineConfiguration.minecraftExecutor = MinecraftClient.getInstance()
     }
