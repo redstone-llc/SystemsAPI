8c79dad9d1c9214374702184cc5a2f0145e19048
Wekend <wekendmc@gmail.com>
2025-11-23 16:08:36 -0800

    LAST THING

--- src/main/kotlin/llc/redstone/systemapi/SystemAPI.kt
+++ src/main/kotlin/llc/redstone/systemapi/SystemAPI.kt
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory
 object SystemAPI : ClientModInitializer {
     val MOD_ID = "systemapi"
     val LOGGER: Logger = LoggerFactory.getLogger("SystemAPI")
-    val VERSION = /*$ mod_version*/ "0.2.3";
+    val VERSION = /*$ mod_version*/ "0.0.1";
     val MINECRAFT = /*$ minecraft*/ "1.21.9";
     val MC: MinecraftClient
         get() = MinecraftClient.getInstance()
