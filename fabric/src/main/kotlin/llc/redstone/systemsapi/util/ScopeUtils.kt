c46be11ef701d4ec0291b07b9808c1d64c74bf11
Wekend <wekendmc@gmail.com>
2025-12-15 14:57:56 -0800

    get rid of this old code

--- fabric/src/main/kotlin/llc/redstone/systemsapi/util/ScopeUtils.kt
+++ fabric/src/main/kotlin/llc/redstone/systemsapi/util/ScopeUtils.kt
@@ -1,15 +1,5 @@
 package llc.redstone.systemsapi.util
 
-import llc.redstone.systemsapi.api.Event
-import llc.redstone.systemsapi.data.EventScope
-
 object ScopeUtils {
-
-    fun isAllowedForEvent(actionClass: Class<*>, event: Event.Events): Boolean {
-        val ann = actionClass.getAnnotation(EventScope::class.java) ?: return true
-        if (ann.only.isNotEmpty()) return ann.only.contains(event)
-        if (ann.except.isNotEmpty()) return !ann.except.contains(event)
-        return true
-    }
 
 }
