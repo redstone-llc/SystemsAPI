4f5ab0c373e81baf422e03a0c6e780d91ba8d2a3
Sin_ender <ender@sinender.net>
2026-01-05 21:36:20 -0500

    chore: Remove Json item nbt fuckery support

--- fabric/src/main/kotlin/llc/redstone/systemsapi/util/ItemUtils.kt
+++ fabric/src/main/kotlin/llc/redstone/systemsapi/util/ItemUtils.kt
@@ -1,12 +1,7 @@
 package llc.redstone.systemsapi.util
 
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.nbt.NbtElement
 import net.minecraft.nbt.NbtOps
 import kotlin.jvm.optionals.getOrNull
 
@@ -20,50 +15,5 @@ object ItemUtils {
     fun createFromNBT(nbt: NbtCompound): ItemStack {
         return ItemStack.CODEC.decode(NbtOps.INSTANCE, nbt).result().getOrNull()?.first
             ?: throw IllegalArgumentException("Failed to decode ItemStack from NBT")
-    }
-
-    fun createFromJsonString(json: String): ItemStack {
-        val jsonObj = JsonParser.parseString(json).asJsonObject
-        val nbt = createNbtFromJsonObject(jsonObj)
-        if (nbt !is NbtCompound) throw IllegalArgumentException("Top level JSON element must be an object")
-        return createFromNBT(nbt)
-    }
-
-    private fun createNbtFromJsonObject(asJsonObject: JsonObject): NbtElement {
-        val nbt = NbtCompound()
-        for ((key, value) in asJsonObject.entrySet()) {
-            if (value.isJsonObject) {
-                nbt.put(key, createNbtFromJsonObject(value.asJsonObject))
-            } else if (value.isJsonPrimitive) {
-                val prim = value.asJsonPrimitive
-                when {
-                    prim.isBoolean -> nbt.putBoolean(key, prim.asBoolean)
-                    prim.isNumber -> nbt.putDouble(key, prim.asDouble)
-                    prim.isString -> nbt.putString(key, prim.asString)
-                }
-            } else if (value.isJsonArray) {
-                nbt.put(key, createNbtFromJsonArray(value.asJsonArray))
-            }
-        }
-        return nbt
-    }
-
-    private fun createNbtFromJsonArray(asJsonArray: JsonArray): NbtElement {
-        val listNbt = NbtCompound()
-        for ((index, element) in asJsonArray.withIndex()) {
-            if (element.isJsonObject) {
-                listNbt.put("$index", createNbtFromJsonObject(element.asJsonObject))
-            } else if (element.isJsonPrimitive) {
-                val prim = element.asJsonPrimitive
-                when {
-                    prim.isBoolean -> listNbt.putBoolean("$index", prim.asBoolean)
-                    prim.isNumber -> listNbt.putDouble("$index", prim.asDouble)
-                    prim.isString -> listNbt.putString("$index", prim.asString)
-                }
-            } else if (element.isJsonArray) {
-                listNbt.put("$index", createNbtFromJsonArray(element.asJsonArray))
-            }
-        }
-        return listNbt
     }
 }
